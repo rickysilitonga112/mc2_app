8a0b07f891a740341253d6d3303b89e55a0fb2dd
Hisyams <syamesanusi@gmail.com>
2022-06-28 22:21:26 +0700

    Membuat layout screen problem view

--- mc2_app/Views/ScreenProblemView.swift
+++ mc2_app/Views/ScreenProblemView.swift
@@ -8,8 +8,88 @@
 import SwiftUI
 
 struct ScreenProblemView: View {
+    let screenWidth = UIScreen.main.bounds.size.width
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center) {
+            ZStack {
+                Text("Apa masalah kulit wajahmu?")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .background()
+                    .frame(width: screenWidth - 54, alignment: .leading)
+                    .padding(28)
+            } .frame(width: screenWidth, height: 184, alignment: .leading)
+                .background(.gray)
+            Spacer()
+            
+            VStack{
+                HStack{
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 235/255, blue: 255/255, opacity: 0.4), Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.13)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 166, height: 234, alignment: .center)
+                        .overlay {
+                            VStack{
+                                Image("Jerawat")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 120)
+                                Text("Wajah Berjerawat")
+                                    .font(.callout)
+                                    .foregroundColor(.white)
+                                    .fontWeight(.medium)
+                                
+                            }.padding()
+                        }
+                    
+                    Spacer()
+                    
+                    RoundedRectangle(cornerRadius: 8)
+                        .fill(Color.gray)
+                        .frame(width: 166, height: 234, alignment: .center)
+                        .overlay {
+                            VStack{
+                                Image("Kusam")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 120)
+                                Text("Wajah Kusam")
+                                    .font(.callout)
+                                    .fontWeight(.medium)
+                                
+                                
+                            }.padding()
+                        }
+                }
+                
+                
+            }
+            Spacer()
+            
+            Button {
+                // do something
+            } label: {
+                Capsule()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 52/255, blue: 2/255), Color(red: 143/255, green: 76/255, blue: 195/255)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .frame(width: 282, height: 50, alignment: .center)
+                    .overlay {
+                        Text("Ayo cek jenis kulitmu")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.bold)
+                    }
+                
+            }
+            Spacer()
+                .frame(height: 30)
+            Button{
+                //sudah tau jenis kulit
+            } label: {
+                Text("Saya sudah tau jenis kulit saya")
+                    .foregroundColor(.black)
+                
+            }
+        }.padding(.horizontal, 28)
+            .background(Color(red: 0/255, green: 20/255, blue: 34/255))
+        
     }
 }
 
@@ -18,3 +98,4 @@ struct ScreenProblemView_Previews: PreviewProvider {
         ScreenProblemView()
     }
 }
+
