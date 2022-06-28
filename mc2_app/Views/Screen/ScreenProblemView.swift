1f3b37441c728a96465dd5ee986550dfe507a481
“rickysilitonga112” <rickysilitonga112@gmail.com>
2022-06-29 02:41:11 +0700

    fix card color and layout

--- mc2_app/Views/Screen/ScreenProblemView.swift
+++ mc2_app/Views/Screen/ScreenProblemView.swift
@@ -8,87 +8,65 @@
 import SwiftUI
 
 struct ScreenProblemView: View {
-    let screenWidth = UIScreen.main.bounds.size.width
+    private let screenWidth = UIScreen.main.bounds.size.width
+    private let screenHeight = UIScreen.main.bounds.size.height
+    
     var body: some View {
-        VStack(alignment: .center) {
-            ZStack {
-                Text("Apa masalah kulit wajahmu?")
-                    .font(.title)
-                    .fontWeight(.semibold)
-                    .background()
-                    .frame(width: screenWidth - 54, alignment: .leading)
-                    .padding(28)
-            } .frame(width: screenWidth, height: 184, alignment: .leading)
-                .background(.gray)
-            Spacer()
+        ZStack {
+            Image("bg_blur")
+                .resizable()
+                .frame(width: screenWidth)
             
             VStack{
-                HStack{
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 235/255, blue: 255/255, opacity: 0.4), Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.13)]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                        .frame(width: 166, height: 234, alignment: .center)
-                        .overlay {
-                            VStack{
-                                Image("Jerawat")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 120)
-                                Text("Wajah Berjerawat")
-                                    .font(.callout)
-                                    .foregroundColor(.white)
-                                    .fontWeight(.medium)
-                                
-                            }.padding()
-                        }
+                ZStack {
+                    Text("Apa masalah kulit \nwajahmu?")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .lineLimit(2)
+                        .foregroundColor(.white)
+                        .frame(width: screenWidth - 56, alignment: .leading)
                     
-                    Spacer()
-                    
-                    RoundedRectangle(cornerRadius: 8)
-                        .fill(Color.gray)
-                        .frame(width: 166, height: 234, alignment: .center)
-                        .overlay {
-                            VStack{
-                                Image("Kusam")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 120)
-                                Text("Wajah Kusam")
-                                    .font(.callout)
-                                    .fontWeight(.medium)
-                                
-                                
-                            }.padding()
-                        }
+                }
+                .frame(height: 180, alignment: .leading)
+                
+                Spacer()
+                
+                HStack(spacing: 12){
+                    SkinProblemCard(skinProblem: "Wajah Berjerawat", imageName: "Jerawat")
+                    SkinProblemCard(skinProblem: "Wajah Kusam", imageName: "Kusam")
                 }
                 
+                Spacer()
                 
-            }
-            Spacer()
+                Button {
+                    // sudah tau jenis kulit
+                } label: {
+                    Capsule()
+                        .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 52/255, blue: 2/255), Color(red: 143/255, green: 76/255, blue: 195/255)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                        .frame(width: 282, height: 50, alignment: .center)
+                        .overlay {
+                            Text("Ayo cek jenis kulitmu")
+                                .foregroundColor(Color.white)
+                                .fontWeight(.bold)
+                        }
+                }
+                Spacer()
+                    .frame(height: 18)
+                
+                Button{
+                    //sudah tau jenis kulit
+                } label: {
+                    Text("Saya sudah tau jenis kulit saya")
+                        .foregroundColor(.white)
+                }
+                
+                Spacer()
+                    .frame(height: 20)
+            }.padding(.horizontal, 28)
+                .background(Color(red: 0/255, green: 20/255, blue: 34/255))
             
-            Button {
-                // do something
-            } label: {
-                Capsule()
-                    .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 52/255, blue: 2/255), Color(red: 143/255, green: 76/255, blue: 195/255)]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                    .frame(width: 282, height: 50, alignment: .center)
-                    .overlay {
-                        Text("Ayo cek jenis kulitmu")
-                            .foregroundColor(Color.white)
-                            .fontWeight(.bold)
-                    }
-                
-            }
-            Spacer()
-                .frame(height: 30)
-            Button{
-                //sudah tau jenis kulit
-            } label: {
-                Text("Saya sudah tau jenis kulit saya")
-                    .foregroundColor(.black)
-                
-            }
-        }.padding(.horizontal, 28)
-            .background(Color(red: 0/255, green: 20/255, blue: 34/255))
+            
+        }
         
     }
 }
@@ -99,3 +77,29 @@ struct ScreenProblemView_Previews: PreviewProvider {
     }
 }
 
+
+struct SkinProblemCard: View {
+    let skinProblem: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 235/255, blue: 255/255).opacity(0.4), Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.13)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+            .opacity(0.26)
+            .frame(width: 156, height: 240, alignment: .center)
+            .overlay {
+                VStack{
+                    Image(imageName)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 120)
+                    Text(skinProblem)
+                        .font(.callout)
+                        .foregroundColor(.white)
+                        .fontWeight(.semibold)
+                        .lineLimit(1)
+                    
+                }
+            }
+    }
+}
