003d7f43612807594aca0ee40c6d9394239129fa
Hisyams <syamesanusi@gmail.com>
2022-07-01 15:39:06 +0700

    membuat identification result view

--- mc2_app/Views/Screens/IdentificationResultView.swift
+++ mc2_app/Views/Screens/IdentificationResultView.swift
@@ -8,33 +8,44 @@
 import SwiftUI
 
 struct IdentificationResultView: View {
+    private let screenWidth =
+    UIScreen.main.bounds.size.width
+    private let screenHeight =
+    UIScreen.main.bounds.size.height
+    
     var body: some View {
         
     ZStack{
-        VStack{
-            Text("Hasil Analisa")
-                .font(.largeTitle)
-                .fontWeight(.medium)
-            Text("""
-Kamu memiliki jenis kulit berminyak,
-dan permasalahan kulit wajahmu
-**Berjerawat**
-""")
-            .multilineTextAlignment(.center)
-            .frame(width: 281, height: 90)
+        VStack(spacing:0){
+            VStack{
+                Text("Hasil Analisa")
+                    .font(.largeTitle)
+                    .fontWeight(.medium)
+                    .foregroundColor(.white)
+                Text("Kamu memiliki jenis kulit berminyak,dan permasalahan kulit wajahmu **Berjerawat**")
+                .multilineTextAlignment(.center)
+                .frame(width: 281, height: 90)
+                .foregroundColor(.white)
+            }
+                .frame(width: screenWidth ,height: 220, alignment: .center)
+                .background(LinearGradient(gradient: Gradient(colors: [
+                    Color(red: 248/255, green: 55/255, blue: 15/255, opacity: 0.37),
+                    Color(red: 0/255, green: 20/255, blue: 34/255),
+                ]), startPoint: .center, endPoint: .bottom))
             
-            RoundedRectangle(cornerRadius: 8)
-                .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.34), Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.13)]), startPoint: .topLeading, endPoint: .bottomTrailing))
-                .frame(width: 198, height: 232)
-                .opacity(0.26)
-                .shadow(radius: 3)
-                .overlay {
-                    ZStack{
-                        Image("Jerawatt")
-                            .resizable()
-                            .frame(width: 124, height: 176)
-                    }
-                }
+            ZStack{
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.40), Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.13)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .opacity(0.26)
+                    .shadow(radius: 3)
+                    .frame(width: 198, height: 232)
+                Image("Jerawatt")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 124)
+            }
+           
+            
             
             Spacer()
                 .frame(height:188)
@@ -52,11 +63,12 @@ dan permasalahan kulit wajahmu
                     }
                 
             }
-            
-            
+            Spacer()
             
         }
     }
+    .frame(width: screenWidth, height: screenHeight)
+    .background(Color(red: 0/255, green: 20/255, blue: 34/255).edgesIgnoringSafeArea(.all))
 }
 
 struct IdentificationResultView_Previews: PreviewProvider {
