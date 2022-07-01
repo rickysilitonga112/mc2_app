a187c81f508ac564f3105da00a4ba37bf8f3344e
Hisyams <syamesanusi@gmail.com>
2022-07-01 11:27:38 +0700

    Membuat identification result view

--- mc2_app/Views/Screens/IdentificationResultView.swift
+++ mc2_app/Views/Screens/IdentificationResultView.swift
@@ -9,7 +9,53 @@ import SwiftUI
 
 struct IdentificationResultView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    ZStack{
+        VStack{
+            Text("Hasil Analisa")
+                .font(.largeTitle)
+                .fontWeight(.medium)
+            Text("""
+Kamu memiliki jenis kulit berminyak,
+dan permasalahan kulit wajahmu
+**Berjerawat**
+""")
+            .multilineTextAlignment(.center)
+            .frame(width: 281, height: 90)
+            
+            RoundedRectangle(cornerRadius: 8)
+                .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.34), Color(red: 255/255, green: 255/255, blue: 255/255, opacity: 0.13)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                .frame(width: 198, height: 232)
+                .opacity(0.26)
+                .shadow(radius: 3)
+                .overlay {
+                    ZStack{
+                        Image("Jerawatt")
+                            .resizable()
+                            .frame(width: 124, height: 176)
+                    }
+                }
+            
+            Spacer()
+                .frame(height:188)
+            
+            Button {
+                // do something
+            } label: {
+                Capsule()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color(red: 255/255, green: 52/255, blue: 2/255), Color(red: 143/255, green: 76/255, blue: 195/255)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .frame(width: 282, height: 50, alignment: .center)
+                    .overlay {
+                        Text("Ayo cek jenis kulitmu")
+                            .foregroundColor(Color.white)
+                            .fontWeight(.bold)
+                    }
+                
+            }
+            
+            
+            
+        }
     }
 }
 
@@ -17,4 +63,5 @@ struct IdentificationResultView_Previews: PreviewProvider {
     static var previews: some View {
         IdentificationResultView()
     }
+}
 }
